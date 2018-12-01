728b20374231566a6bee3d8650d58faabf834a06
Patrick Zinner <zinnsoldat91@gmail.com>
2018-12-01 17:06:54 +0100

    include easter based holidays in "getAllHolidays"

--- austrialib/src/main/java/com/patrickzinner/austrialib/holiday/HolidayCalendar.java
+++ austrialib/src/main/java/com/patrickzinner/austrialib/holiday/HolidayCalendar.java
@@ -1,11 +1,13 @@
 package com.patrickzinner.austrialib.holiday;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.patrickzinner.austrialib.enums.State;
 
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Class for calculating public holidays of Austria
@@ -35,10 +37,35 @@ public class HolidayCalendar {
     public List<Holiday> getAllHolidays(int year) {
         List<Holiday> holidays = new ArrayList<>();
         fixedHolidays.stream().map(fixedHoliday -> convert(fixedHoliday, year)).forEach(holidays::add);
-
         LocalDate easterSunday = easterBasedHolidayCalculator.calculateEasterSunday(year);
-
+        holidays.addAll(calculateEasterBasedHolidays(easterSunday));
         return holidays;
+    }
+
+    private List<Holiday> calculateEasterBasedHolidays(LocalDate easterSunday) {
+        List<Holiday> result = new ArrayList<>();
+        LocalDate easterMonday = easterBasedHolidayCalculator.calculateEasterMonday(easterSunday);
+        LocalDate ascensionDay = easterBasedHolidayCalculator.calculateAscensionDay(easterSunday);
+        LocalDate pentecostDay = easterBasedHolidayCalculator.calculatePentecost(easterSunday);
+        LocalDate corpusChristi = easterBasedHolidayCalculator.calculateCorpusChristi(easterSunday);
+        LocalDate goodFriday = easterBasedHolidayCalculator.calculateGoodFriday(easterSunday);
+        result.add(createHoliday(easterSunday, "Easter Sunday", "Ostersonntag", true));
+        result.add(createHoliday(easterMonday, "Easter Monday", "Ostermontag", true));
+        result.add(createHoliday(ascensionDay, "Ascension of Jesus", "Christi Himmelfahrt", true));
+        result.add(createHoliday(pentecostDay, "Pentecost", "Pfingstmontag", true));
+        result.add(createHoliday(corpusChristi, "Corpus Christi", "Fronleichnam", true));
+        result.add(createHoliday(goodFriday, "Good Friday", "Karfreitag", false));
+        return result;
+    }
+
+    private Holiday createHoliday(LocalDate date, String enName, String deName, boolean legalHoliday) {
+        Holiday holiday = new Holiday();
+        holiday.setStates(Arrays.stream(State.values()).collect(Collectors.toSet()));
+        holiday.setLegalHoliday(legalHoliday);
+        holiday.setDate(date);
+        holiday.setEnName(enName);
+        holiday.setDeName(deName);
+        return holiday;
     }
 
 
